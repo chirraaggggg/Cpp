d010562d6196ac21567d05ff5c7c9529d1b74613
Chirag <chhirag920@gmail.com>
2025-09-16 22:14:31 +0530

    learning

--- functions/nCr.cpp
+++ functions/nCr.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int factorial(int n){
-    
+    int fact = 1;
+
+    for (int i = 1; i <=n; i++){
+        fact = fact * i;
+    }
+    return fact;
+}
+int nCr(int n, int r){
+    int num = factorial(n);
+    int deno = factorial(r) * factorial(n-r);
+    int ans = num/deno;
+    return ans;
+}
+int main(){
+    int n;
+    int r;
+    cin >> n >> r;
+
+    int answer = nCr(n,r);
+    cout << "answer is: "<< answer <<endl;
+
+    return 0;
 }
