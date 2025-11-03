42ca5eba087000d3c442cdd067d5890a50161eda
Chirag <chhirag920@gmail.com>
2025-11-03 23:35:34 +0530

    getting the length of a string

---
+++ Strings/length.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int getLenght(char name[]){
+    int count = 0;
+    for(int i = 0; name[i] != 0; i++){
+        count++;
+    }
+    return count;
+}
+int main(){
+    
+}
