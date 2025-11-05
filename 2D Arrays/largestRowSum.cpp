457183950427f4c5e01704d639a36d0383556587
Chirag <chhirag920@gmail.com>
2025-11-05 10:58:42 +0530

    largest row sum

--- 2D Arrays/largestRowSum.cpp
+++ 2D Arrays/largestRowSum.cpp
@@ -1,10 +1,14 @@
+// calculate the sum of row
+// find the row which has max sum
+
+
 #include<iostream>
 using namespace std;
 
 int largestRowSum(int arr[][3], int row, int col){
 
-    int max = INT_MIN;
-
+    int max = INT_MIN; //calculating the max sum of a row
+    int rowIndex = -1;
     for(int row=0; row<3; row++){
         int sum = 0;
         for(int col=0; col<3; col++){
@@ -12,7 +16,19 @@ int largestRowSum(int arr[][3], int row, int col){
         }
         if(sum > max){
             max = sum;
+            rowIndex = row; // rowIndex wo hoga jispe loop chal rha hai
         }
     }
     cout << "maximum sum is: " << max << endl;
+    return row;
+}
+int main(){
+    int arr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    largestRowSum(arr, 3,3);
+
+    return 0;
 }
